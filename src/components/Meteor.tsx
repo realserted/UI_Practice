f4e977a8cd0a573691283564fa78f1418f8dcabd
LesterMYT <lester.sanchez@module-zero.com>
2024-10-23 19:52:37 +0800

    feat: added icon cloud

--- src/components/Meteor.tsx
+++ src/components/Meteor.tsx
@@ -1,6 +1,7 @@
 import Meteors from "@/components/ui/meteors";
 import { ShinyButtonDemo } from "@/components/ShinyButton";
 import { BlurInDemo } from "./BlurInTexts";
+import { DockDemo } from "./Docks";
 export function MeteorDemo() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
@@ -8,6 +9,7 @@ export function MeteorDemo() {
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10" >
       <BlurInDemo />
       </span>
+      <div className="flex flex-col absolute bottom-32 "><DockDemo /></div>
       <div className="flex flex-col absolute bottom-12 "><ShinyButtonDemo /></div>
     </div>
     
--- src/pages/Lester.tsx
+++ src/pages/Lester.tsx
@@ -2,7 +2,7 @@ import { DotPatternDemo } from "@/components/DotPattern";
 import { MarqueeDemo } from "@/components/MarqueeHorizontal";
 import { BentoDemo } from "@/components/BentoGrid";
 import { MeteorDemo } from "@/components/Meteor";
-
+import { IconCloudDemo } from "@/components/IconClouds";
 
 const Lester = () => {
   return (
@@ -10,6 +10,7 @@ const Lester = () => {
       <MeteorDemo />
       <MarqueeDemo />
       <BentoDemo />
+      <IconCloudDemo />
     </div>
   );
 };
