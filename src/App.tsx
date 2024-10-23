fb5c9c46dfe847efe9fd60fe87bef650513dde1b
LesterMYT <lester.sanchez@module-zero.com>
2024-10-23 13:26:16 +0800

    feat: added various components from magic UI also added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
- 
+import { DotPatternDemo } from "./components/DotPattern";
+import { MarqueeDemo } from "./components/MarqueeHorizontal";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "./components/mode-toggle";
+
+
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col">
+      <div className="flex self-end"><ModeToggle/></div>
+      <DotPatternDemo/>
+      <MarqueeDemo/>
+      </div>
+      </ThemeProvider>
   );
 }
